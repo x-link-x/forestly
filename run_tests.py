54469f6885d9d46a83e49bc69e97f59e5b4a8303
Link <xlinkx_@outlook.com>
2021-06-08 16:49:36 +0100

    changed directory structure

---
+++ run_tests.py
@@ -0,0 +1,7 @@
+import unittest
+from tests.tree_test import *
+from tests.area_test import *
+from tests.treetype_test import *
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/treetype_test.py
@@ -0,0 +1,17 @@
+import unittest
+from forest.models.treetype import TreeType
+
+class TestTreeType(unittest.TestCase):
+    def setUp(self):
+        self.tree_type = TreeType("Oak")
+
+    def test_tree_type_has_name(self):
+        expected = "Oak"
+        actual = self.tree_type.name
+        self.assertEqual(expected, actual)
+
+    def test_tree_type_get_locations_returns_a_list(self):
+        expected = []
+        actual = self.tree_type.get_locations()
+        self.assertEqual(expected, actual)
+
