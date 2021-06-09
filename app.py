bbaa21a1cf203c222de7fed3c6102049d2a499c7
Link <xlinkx_@outlook.com>
2021-06-09 17:00:40 +0100

    added routes for home page, links to manage objects

--- app.py
+++ app.py
@@ -9,5 +9,18 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+        return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/forest")
+def forest():
+    return render_template("forest.html")
+
+
 if __name__ == '__main__':
     app.run()
